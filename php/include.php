9d82dc80fa6d88efa9c47f17bdd4551b481fd9b4
castaneai <castaneai@castaneai.net>
2018-10-29 14:27:25 +0900

    Add auth cache

--- php/include.php
+++ php/include.php
@@ -2,7 +2,10 @@
 require __DIR__.'/vendor/autoload.php';
 
 use Google\Cloud\Spanner\SpannerClient;
+use Google\Cloud\Spanner\Session\SessionPoolInterface;
 use Psr\Cache\CacheItemPoolInterface;
+use Google\Cloud\Spanner\Session\CacheSessionPool;
+use Google\Auth\Cache\SysVCacheItemPool;
 
 function startTrace()
 {
@@ -13,18 +16,35 @@ function startTrace()
         ]];
     });
 
-    opencensus_trace_method(\Grpc\Call::class, 'startBatch', function($scope, $args) {
-        return ['attributes' => ['args' => json_encode($args)]];
+    opencensus_trace_method(\Grpc\Call::class, 'startBatch', function($scope, $batch) {
+        return ['attributes' => ['batch' => json_encode($batch)]];
     });
 
     \OpenCensus\Trace\Tracer::start(new \OpenCensus\Trace\Exporter\OneLineEchoExporter());
 }
 
-function spannerContext(Closure $func, CacheItemPoolInterface $authCache = null, CacheItemPoolInterface $sessionPoolCache = null)
+function spannerContext(Closure $func, CacheItemPoolInterface $authCache = null, SessionPoolInterface $sessionPool = null)
 {
     $instanceId = getenv('SPANNER_INSTANCE_ID');
     $databaseId = getenv('SPANNER_DATABASE_ID');
-    $spanner = new SpannerClient();
-    $db = $spanner->connect($instanceId, $databaseId);
+
+    if ($authCache === null) {
+        $authCache = new SysVCacheItemPool();
+    }
+    $spannerClientOptions = [
+        'authCache' => $authCache,
+    ];
+    $spanner = new SpannerClient($spannerClientOptions);
+
+    if ($sessionPool === null) {
+        $sessionCacheItemPool = new SysVCacheItemPool();
+        $sessionPool =  new CacheSessionPool($sessionCacheItemPool, [
+            'minSession' => 10,
+        ]);
+    }
+    $databaseOptions = [
+        'sessionPool' => $sessionPool,
+    ];
+    $db = $spanner->connect($instanceId, $databaseId, $databaseOptions);
     return $func($db);
 }
