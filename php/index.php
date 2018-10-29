3e4cd59e9be391a0520176cb74e202c46b23fefe
castaneai <castaneai@castaneai.net>
2018-10-29 11:41:07 +0900

    Add cloud spanner codes

--- php/index.php
+++ php/index.php
@@ -1,2 +1,13 @@
 <?php
-echo 'hello';
+require __DIR__.'/vendor/autoload.php';
+
+use Google\Cloud\Spanner\SpannerClient;
+
+$instanceId = getenv('SPANNER_INSTANCE_ID');
+$databaseId = getenv('SPANNER_DATABASE_ID');
+
+$spanner = new SpannerClient();
+$db = $spanner->connect($instanceId, $databaseId);
+
+$row = $db->execute('SELECT "Hello, world"')->rows()->current();
+var_dump($row);
